ec9c5193edb7e6a45800478702d838322f87dace
Juan Arias <juanestebanariasorozco378@gmail.com>
2022-12-09 14:31:42 -0600

    Creacion del repositorio

---
+++ menu.js
@@ -0,0 +1,27 @@
+var menu = document.querySelector('.nav-menu');
+var m1 = document.querySelector('.m1');
+var m2 = document.querySelector('.m2');
+var m3 = document.querySelector('.m3');
+var m4 = document.querySelector('.m4');
+var m5 = document.querySelector('.m5');
+
+
+m1.addEventListener("click", ()=> {
+    menu.classList.add("ocultar");
+})
+  
+m2.addEventListener("click", ()=> {
+    menu.classList.add("ocultar");
+})
+  
+m3.addEventListener("click", ()=> {
+    menu.classList.add("ocultar");
+})
+
+m4.addEventListener("click", ()=> {
+    menu.classList.add("ocultar");
+})
+
+m5.addEventListener("click", ()=> {
+    menu.classList.add("ocultar");
+})
